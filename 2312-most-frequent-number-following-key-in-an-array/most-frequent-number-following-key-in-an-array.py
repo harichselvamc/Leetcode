a89046b1ca4fe220ef309e26b4513d698b39b1db
harichselvam <90306857+harichselvamc@users.noreply.github.com>
2026-08-01 01:41:01 +0530

    Time: 4 ms (36.17%) | Memory: 12.5 MB (44.68%) - LeetSync

--- 2312-most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.py
+++ 2312-most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.py
@@ -5,30 +5,27 @@ class Solution(object):
         :type key: int
         :rtype: int
         """
+        # nums = [1,100,200,1,100]
         temp={}
+        
         for i in range(len(nums)-1):
-            if key==nums[i]:
+            if nums[i]==key:
                 nextnumber=nums[i+1]
                 temp[nextnumber]=temp.get(nextnumber,0)+1
+        
+        #{100: 2}
 
         max_value=0
 
         for value in temp.values():
             if value>max_value:
                 max_value=value
+        
 
+        # max_value=2
 
 
         for key,value in temp.items():
-
-            if value==max_value:
-
+            if max_value==value:
                 return key
-                
-
-
-
-
-
-
 
